28cb1d5fb804e5ca597600fe55189e453370d11e
Bouzir Mohamed Yacine <77847874+yacine1605@users.noreply.github.com>
2025-09-18 21:52:06 +0100

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -43,10 +43,9 @@ export default function SiteHeader() {
           </Link>
         </nav>
         <div className="hidden md:block">
-          <LanguageToggle />
           <Button asChild className="bg-blue-400 hover:bg-blue-700">
             <Link href="/depannage" target="_blank" rel="noopener noreferrer">
-              {t("joinAsPro")}{" "}
+              {t("joinAsPro")}
             </Link>
           </Button>
         </div>
@@ -55,13 +54,22 @@ export default function SiteHeader() {
           className="md:hidden inline-flex items-center justify-center rounded-md p-2 hover:bg-muted"
           onClick={() => setOpen((o) => !o)}
         >
-          {open ? <X className="h-5 w-5" /> : <Menu className="h-5 w-5" />}
+          {open ? (
+            <X className="h-5 w-5" />
+          ) : (
+            <div className="flex flex-row gap-2 items-center">
+              <Menu className="h-5 w-5" />
+              <div className="flex "></div>
+            </div>
+          )}
         </button>
       </div>
       {open && (
         <div className="md:hidden border-t">
           <div className="container py-3 flex flex-col gap-3">
-           
+            <Link href="/" className="py-1" onClick={() => setOpen(false)}>
+              Accueil
+            </Link>
             <Link href="/cgu" className="py-1" onClick={() => setOpen(false)}>
               {t("terms")}
             </Link>
